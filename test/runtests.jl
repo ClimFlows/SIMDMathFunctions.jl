881fa53fc730682c69ed69a4c2ac79b09d68c711
Thomas Dubos <thomas.dubos@lmd.ipsl.fr>
2023-11-06 08:39:49 +0100

    Increase coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
-using SIMDFastMath: SIMD, tolerance, fast_functions, is_supported, is_fast
+using SIMDFastMath:
+    SIMD, tolerance, fast_functions, is_supported, is_fast, vmap_unop, vmap_binop
 using Test
 
 @info "Fast functions"
@@ -10,17 +11,35 @@ data(F, N, ::Function) = range(F(0.01), F(0.9), length = N)
 data(F, N, ::typeof(acosh)) = range(F(1.1), F(1.9), length = N)
 data(F, N, ::typeof(@fastmath acosh)) = range(F(1.1), F(1.9), length = N)
 
+data_binop(F, N, ::Function) =
+    range(F(0.01), F(0.9), length = N), range(F(0.01), F(0.9), length = N)
+
 function validate(res::SIMD.Vec, ref, tol)
-    ref = SIMD.Vec(ref...)
     err = relative_error(res, ref)
-    ref, err, any(err > tol)
+    err, any(err > tol)
 end
 function validate(res::Tuple, ref, tol)
-    ref = map(x -> SIMD.Vec(x...), Tuple(zip(ref...)))
     err = map(relative_error, res, ref)
-    ref, err, any(map(err -> any(err > tol), err))
+    err, any(map(err -> any(err > tol), err))
 end
 relative_error(res, ref) = abs(res - ref) / abs(ref)
+
+for fun in sort(fast_functions(2), by = string) # two-argument functions
+    @assert is_supported(fun)
+    @assert is_fast(fun)
+    tol = tolerance(fun)
+    tol = tolerance(fun)
+    @testset "$(string(fun))" begin
+        for F in (Float32, Float64), N in (4, 8, 16, 32)
+            x, y = data_binop(F, N, fun)
+            x, y = SIMD.Vec(x...), SIMD.Vec(x...)
+            res, ref = fun(x, y), vmap_binop(fun, x, y)
+            err, fail = validate(res, ref, tol * eps(F))
+            fail && @warn fun arg ref res err
+            @test !fail
+        end
+    end
+end
 
 for fun in sort(fast_functions(1), by = string) # one-argument functions
     @assert is_supported(fun)
@@ -28,9 +47,9 @@ for fun in sort(fast_functions(1), by = string) # one-argument functions
     tol = tolerance(fun)
     @testset "$(string(fun))" begin
         for F in (Float32, Float64), N in (4, 8, 16, 32)
-            d = data(F, N, fun)
-            res = fun(SIMD.Vec(d...))
-            ref, err, fail = validate(res, map(fun, d), tol * eps(F))
+            d = SIMD.Vec(data(F, N, fun)...)
+            res, ref = fun(d), vmap_unop(fun, d)
+            err, fail = validate(res, ref, tol * eps(F))
             fail && @warn fun arg ref res err
             @test !fail
         end
